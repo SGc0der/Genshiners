7cff8247dca26b4b4da10ccff90443a04f3d6d30
sC0der <102285034+SGc0der@users.noreply.github.com>
2023-05-06 16:23:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function relocate() {
+    window.location = "home.html";
+}
+function storyline() {
+    window.location = "storyline.html";
+}
+function monsters() {
+    window.location = "monsters.html";
+}
+function characters() {
+    window.location = "characters.html";
+}
+// regions
